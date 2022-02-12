42044dd964d57389c4e8bb798c9f0a5676495191
sriyanka-21 <99538039+sriyanka-21@users.noreply.github.com>
2022-02-12 21:24:00 +0530

    Update lift off c question 1 assignment 1.c

--- lift off c question 1 assignment 1.c
+++ lift off c question 1 assignment 1.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 int main()
 {
-printf("Name:Sriyanka Satapathy\n");
-printf("registration number=085\n");
-printf("branch:Electronics and Telecommunication\n");
-printf("hobbies:reading novels and singing");
+ int reg;
+  char name[30],branch[50],hobbies[50];
+ printf("enter name=\n");
+  scanf("%s",&name);
+  printf("enter branch=\n");
+  scanf("%s",&branch);
+  printf("enter registration numberr(only last three digits)=\n");
+  scanf("%d",&reg);
+  printf("enter hobbies=\n");
+  scanf("%s",&hobbies);
+  printf("Name:%s\n",name);
+printf("registration number(only last three digits)=%d\n",reg);
+printf("branch:%s\n",branch);
+printf("hobbies:%s",hobbies);
+  
 return 0;
 }
